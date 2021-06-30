24d0ff3fb54ec488a02b0ee11cbb39030ee7031a
Murilo <murilo.henriquecunha551@gmail.com>
2021-06-30 02:07:55 -0300

    Fix social buttons

--- src/components/SocialInfo/index.tsx
+++ src/components/SocialInfo/index.tsx
@@ -13,41 +13,41 @@ import { SiBehance } from "react-icons/si";
 
 const SocialInfo: React.FC = () => {
   return (
-    <div className="flex flex-wrap -mx-4 overflow-hidden">
-      <div className="flex items-center justify-center w-1/2 h-10 px-4 my-4 overflow-hidden md:h-20 bg-background-social sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialFacebook size={30} color="#5A5A5A" />
+    <div className="flex flex-wrap -mx-5 overflow-hidden sm:-mx-4 md:-mx-4 xl:-mx-4">
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialFacebook className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-1/2 px-4 my-4 overflow-hidden bg-background-social sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialPinterest size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialPinterest className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-1/2 px-4 my-4 overflow-hidden bg-background-social md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialVimeo size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialVimeo className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-1/2 h-10 px-4 my-4 overflow-hidden md:h-20 bg-background-social md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialDribbble size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialDribbble className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-1/2 px-4 my-4 overflow-hidden bg-background-social md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialTwitter size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialTwitter className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-1/2 px-4 my-4 overflow-hidden bg-background-social md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <SiBehance size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialInstagram className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-1/2 h-10 px-4 my-4 overflow-hidden md:h-20 bg-background-social md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialInstagram size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialYoutube className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-1/2 px-4 my-4 overflow-hidden bg-background-social md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialYoutube size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <TiSocialGooglePlus className="w-10 h-10" color="#5A5A5A" />
       </div>
 
-      <div className="flex items-center justify-center w-full px-4 my-4 overflow-hidden bg-background-social md:w-1/3 lg:w-1/3 xl:w-1/3">
-        <TiSocialGooglePlus size={30} color="#5A5A5A" />
+      <div className="flex items-center justify-center w-1/3 px-5 my-5 overflow-hidden h-1/3 sm:my-4 sm:px-4 sm:w-1/3 md:my-4 md:px-4 md:w-1/3 lg:w-1/3 xl:my-4 xl:px-4 xl:w-1/3">
+        <SiBehance className="w-10 h-10" color="#5A5A5A" />
       </div>
     </div>
   );
