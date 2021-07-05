c2bde07ca8ee767a8686807c7be332d0427a7814
Murilo Cunha <murilo.henriquecunha551@gmail.com>
2021-07-05 10:47:53 -0300

    Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,25 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 module.exports = {
   siteMetadata: {
     title: "Fashion Blog",
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-simple-analytics",
+      options: {
+        trackPageViews: true,
+      },
+    },
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-source-graphcms',
+      resolve: "gatsby-source-graphcms",
       options: {
         endpoint: process.env.GRAPHCMS_ENDPOINT,
         token: process.env.GRAPHCMS_TOKEN,
